f976f1d5d2dea19ca3d08bc5d2723364cada4cc7
shruti_Chauhan <chauhanshruti932@gmail.com>
2025-12-20 16:53:38 +0530

    little changes made

--- navbar.js
+++ navbar.js
@@ -1,22 +1,12 @@
-document.addEventListener("click", function (e) {
 
+  const hamburger = document.querySelector(".hamburger");
   const mobileMenu = document.querySelector(".mobile-menu");
-  const dropdowns = document.querySelectorAll(".mobile-dropdown");
+  const closeBtn = document.querySelector(".close");
 
-  // OPEN MENU
-  if (e.target.closest(".hamburger")) {
-    mobileMenu.classList.add("active");
-  }
+  hamburger.addEventListener("click", () => {
+    mobileMenu.style.right = "0";
+  });
 
-  // CLOSE MENU + RESET DROPDOWNS
-  if (e.target.closest(".close-menu")) {
-    mobileMenu.classList.remove("active");
-    dropdowns.forEach(d => d.classList.remove("active"));
-  }
-
-  // TOGGLE DROPDOWN ONLY ON ARROW
-  if (e.target.classList.contains("toggle-arrow")) {
-    e.target.closest(".mobile-dropdown").classList.toggle("active");
-  }
-
-});
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.style.right = "-100%";
+  });
